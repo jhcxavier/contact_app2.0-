259dd88d684d54971ce85d13fb3e01975452317e
João Henrique Xavier <jhcxavier@gmail.com>
2019-12-17 18:53:31 -0500

    add contact

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import MikePhoto from "../../img/m101.jpg";
 import { Context } from "../store/appContext";
@@ -26,12 +26,13 @@ export const ContactCard = props => {
 						</div>
 						<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 							<div className=" float-right">
-								<Link to={}>
-								<button className="btn">
-									<i className="fas fa-pencil-alt mr-3" />
-								</button>
+								<Link to={"/add"}>
+									<button className="btn">
+										<i className="fas fa-pencil-alt mr-3" />
+									</button>
 								</Link>
-								<button className="btn" onClick={() => props.onDelete()}>
+
+								<button className="btn" onClick={() => alert("test")}>
 									<i className="fas fa-trash-alt" />
 								</button>
 							</div>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,4 @@
+const url = "https://assets.breatheco.de/apis/fake/contact/";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
@@ -5,8 +6,43 @@ const getState = ({ getStore, setStore }) => {
 		},
 		actions: {
 			loadContact() {
-				const url = "https://assets.breatheco.de/apis/fake/contact/agenda/downtown_xii";
-				fetch(url)
+				fetch(url + "agenda/downtown_xii")
+					.then(response => response.json())
+					.then(result => {
+						console.log("result", result),
+							setStore({
+								contacts: result
+							});
+					})
+					.catch(e => console.error(e));
+			},
+			addContact(name, phone, email, address) {
+				fetch(url, {
+					method: "post",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						email: email,
+						agenda_slug: "downtown_xii"
+					})
+				}).then(() => {
+					fetch(url + "agenda/downtown_xii")
+						.then(response => response.json())
+						.then(result => {
+							console.log("result", result),
+								setStore({
+									contacts: result
+								});
+						})
+						.catch(e => console.error(e));
+				});
+			},
+			deleteContact(id) {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "delete"
+				})
 					.then(response => response.json())
 					.then(result => {
 						console.log("result", result),
