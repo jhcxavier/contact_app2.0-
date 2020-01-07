9af3b6c791d146c7bd01554ea688720dc6910af3
João Henrique Xavier <jhcxavier@gmail.com>
2020-01-06 20:51:05 -0500

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,6 +3,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
+	const [email, setEmail]= useState("");
+	const [password, setPassword] = useState("");
 	return (
 		<div className="text-center mt-5">
 			<p>Email</p>
