56be29d7133c3d7d86b863eabbd439b2e3fc8be4
João Henrique Xavier <50052583+jhcxavier@users.noreply.github.com>
2020-01-25 19:24:48 -0500

    Update appContext.js

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -5,7 +5,7 @@ import getState from "./flux.js";
 export const Context = React.createContext(null);
 
 // This function injects the global store to any view/component where you want to use it, we will inject the context to Layout.jsx, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.jsx#L35
+
 const injectContext = PassedComponent => {
 	const StoreWrapper = props => {
 		
